1b11662341a61272030f87c5d4c86933a4e6d52a
Kouhei Yanagita <yanagi@shakenbu.org>
2024-12-22 17:56:28 +0900

    TypeScriptのエラーを修正

--- app/typescript/controllers/application.ts
+++ app/typescript/controllers/application.ts
@@ -2,6 +2,12 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
+declare global {
+  interface Window {
+    Stimulus: typeof application;
+  }
+}
+
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
--- app/typescript/controllers/hello_controller.ts
+++ app/typescript/controllers/hello_controller.ts
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["name", "output"];
+  declare nameTarget: HTMLInputElement;
+  declare outputTarget: HTMLElement;
 
   connect() {
     console.log("Hello, Stimulus!");
