76cf2f6ad85674a62618785bb1c0ba785a853fc8
Kouhei Yanagita <yanagi@shakenbu.org>
2025-02-11 15:31:45 +0900

    ペーパー結果一覧ページを作成

--- app/controllers/admin/round1/results_controller.rb
+++ app/controllers/admin/round1/results_controller.rb
@@ -2,7 +2,9 @@ module Admin
   module Round1
     class ResultsController < ApplicationController
       def index
-        @results = YontakuPlayerResult.preload(player: :player_profile).order(:rank)
+        @players = Player.preload(:yontaku_player_result, :player_profile).sort_by do
+          [it.yontaku_player_result&.rank || Float::INFINITY, it.id]
+        end
       end
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,47 @@
 module ApplicationHelper
+  # @rbs rank Integer
+  # @rbs return bool
+  def round2_seeded?(rank)
+    (1..Matchmaking::Round2::NUM_SEED_PLAYERS).cover?(rank)
+  end
+
+  # @rbs rank Integer
+  # @rbs return bool
+  def round2_advantage?(rank)
+    first = Matchmaking::Round2::NUM_SEED_PLAYERS + 1
+    num_advantaged_players = MatchRule::Round2::NUM_ADVANTAGED_PLAYERS * Round::ROUND2.matches.size
+    (first...first+num_advantaged_players).cover?(rank)
+  end
+
+  # @rbs rank Integer
+  # @rbs return bool
+  def round2_normal?(rank)
+    first = Matchmaking::Round2::NUM_SEED_PLAYERS + (MatchRule::Round2::NUM_ADVANTAGED_PLAYERS * Round::ROUND2.matches.size) + 1
+    num_normal = (MatchRule::Round2::NUM_BUTTONS - MatchRule::Round2::NUM_ADVANTAGED_PLAYERS) * Round::ROUND2.matches.size
+    (first...first+num_normal).cover?(rank)
+  end
+
+  # @rbs rank Integer
+  # @rbs return bool
+  def round2_waiting?(rank)
+    first = Matchmaking::Round2::NUM_SEED_PLAYERS + (MatchRule::Round2::NUM_BUTTONS * Round::ROUND2.matches.size) + 1
+    num_waiting = (MatchRule::Round2::NUM_SEATS - MatchRule::Round2::NUM_BUTTONS) * Round::ROUND2.matches.size
+    (first...first+num_waiting).cover?(rank)
+  end
+
+  # @rbs rank Integer
+  # @rbs return String
+  def round2_rank_color_class(rank)
+    if round2_seeded?(rank)
+      "is-danger"
+    elsif round2_advantage?(rank)
+      "is-info"
+    elsif round2_normal?(rank)
+      "is-warning"
+    elsif round2_waiting?(rank)
+      "is-success"
+    else
+      ""
+    end
+  end
 end
