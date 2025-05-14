0d45fc09252f3d371512688faea092072b116b2a
Kouhei Yanagita <yanagi@shakenbu.org>
2025-05-14 16:29:15 +0900

    CSRF対策で、OriginとSec-Fetch-Siteヘッダをチェックするようにした
    ref. https://speakerdeck.com/corocn/kaigionrails-2024-csrf?slide=48

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,30 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :validate_sec_fetch_site
+
+  private
+
+  ALLOWED_ORIGINS = [] # TODO
+
+  def validate_origin
+    return if request.get? || request.head?
+
+    origin = request.headers["Origin"]
+    unless origin.present? && ALLOWED_ORIGINS.include?(origin)
+      raise CsrfProtectionError
+    end
+  end
+
+  SEC_FETCH_SITE_ALLOWED_VALUES = ["same-origin", "same-site"]
+
+  def validate_sec_fetch_site
+    return if request.get? || request.head?
+
+    sec_fetch_site = request.headers["Sec-Fetch-Site"]
+    if sec_fetch_site.present? && SEC_FETCH_SITE_ALLOWED_VALUES.exclude?(sec_fetch_site.downcase)
+      raise CsrfProtectionError
+    end
+  end
 end
---
+++ app/models/csrf_protection_error.rb
@@ -0,0 +1,2 @@
+class CsrfProtectionError < StandardError
+end
