c17623e88128e3d586e40964df72d6903b4ddfa7
Kouhei Yanagita <yanagi@shakenbu.org>
2025-05-17 09:04:44 +0900

    問い読みの音声再生を実装

--- app/typescript/controllers/quiz_reader_controller.ts
+++ app/typescript/controllers/quiz_reader_controller.ts
@@ -1,7 +1,81 @@
 import { Controller } from "@hotwired/stimulus";
 import { Turbo } from "@hotwired/turbo-rails";
 
-type VoiceStatus = "STOP" | "PLAYING";
+type VoiceStatus = "STANDBY" | "PLAYING" | "PAUSED";
+
+// 「問題」と問題文の間の空白時間の長さ（ms）
+const INTERVAL_AFTER_MONDAI_MS = 300;
+const audioContext = new AudioContext();
+const audioCache = new Map<string, AudioBuffer>();
+
+async function loadAudio(url: string): Promise<AudioBuffer> {
+  //await new Promise((resolve) => setTimeout(resolve, 1000)); // DEBUG
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+  return await audioContext.decodeAudioData(arrayBuffer);
+}
+
+type QuestionReadingContext = {
+  start(): void;
+  stop(): void;
+};
+
+function createQuestionReadingContext(soundId: string): QuestionReadingContext {
+  let currentSource: AudioBufferSourceNode | undefined;
+  const abortController = new AbortController();
+
+  function playAudioBuffer(audioBuffer: AudioBuffer): Promise<void> {
+    return new Promise((resolve, reject) => {
+      abortController.signal.addEventListener("abort", reject);
+      currentSource = audioContext.createBufferSource();
+      currentSource.buffer = audioBuffer;
+      currentSource.connect(audioContext.destination);
+      // note: onended は stop() が呼ばれたときも呼ばれる
+      currentSource.onended = () => {
+        resolve();
+      };
+      currentSource.start();
+    });
+  }
+
+  return {
+    async start() {
+      let mondaiAudioBuffer = audioCache.get("mondai");
+      if (!mondaiAudioBuffer) {
+        const audioUrl = "/sample/mondai.wav";
+        mondaiAudioBuffer = await loadAudio(audioUrl);
+        audioCache.set("mondai", mondaiAudioBuffer);
+      }
+
+      let audioBuffer = audioCache.get(soundId);
+      if (!audioBuffer) {
+        const audioUrl = "/sample/question.wav"; // TODO
+        audioBuffer = await loadAudio(audioUrl);
+        audioCache.set(soundId, audioBuffer);
+      }
+
+      try {
+        await playAudioBuffer(mondaiAudioBuffer);
+        await new Promise((resolve, reject) => {
+          abortController.signal.addEventListener("abort", reject);
+          setTimeout(resolve, INTERVAL_AFTER_MONDAI_MS);
+        });
+        await playAudioBuffer(audioBuffer);
+      } catch (e) {
+        if (e instanceof Error) {
+          console.error(e);
+        }
+      }
+    },
+    stop() {
+      if (currentSource) {
+        currentSource.stop();
+        currentSource = undefined;
+      }
+      abortController.abort();
+    },
+  };
+}
 
 export default class extends Controller {
   static targets = ["isOnAir", "onAirLabel"];
@@ -9,7 +83,8 @@ export default class extends Controller {
   declare isOnAirTarget: HTMLInputElement;
   declare onAirLabelTarget: HTMLElement;
 
-  voiceStatus: VoiceStatus = "STOP";
+  voiceStatus: VoiceStatus = "STANDBY";
+  readingContext: QuestionReadingContext = createQuestionReadingContext("");
 
   connect() {
     console.log("QuizReaderController connected");
@@ -25,17 +100,21 @@ export default class extends Controller {
 
   startReading() {
     if (!this.isOnAirTarget.checked) return;
-    if (this.voiceStatus !== "STOP") return;
+    if (this.voiceStatus !== "STANDBY") return;
 
     console.log("startReading");
     this.voiceStatus = "PLAYING";
+    this.readingContext.start();
+    console.log("startReading done");
   }
 
-  stopReading() {
+  pauseReading() {
     if (this.voiceStatus !== "PLAYING") return;
 
-    console.log("stopReading");
-    this.voiceStatus = "STOP";
+    console.log("pauseReading");
+    this.voiceStatus = "PAUSED";
+
+    this.readingContext.stop();
   }
 
   async switchToQuestion() {
