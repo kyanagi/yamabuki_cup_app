37e60c705e8df132dbdb7bf048aa8b0267db0704
Kouhei Yanagita <yanagi@shakenbu.org>
2025-06-01 17:49:09 +0900

    preloadの追加

--- app/controllers/admin/round2/matches_controller.rb
+++ app/controllers/admin/round2/matches_controller.rb
@@ -4,7 +4,12 @@ module Admin
       include MatchInstanceVariables
 
       def show
-        @match = Round::ROUND2.matches.find_by!(match_number: params[:match_number])
+        @match = Round::ROUND2.matches
+          .preload(last_score_operation: [
+            { question_result: { question_player_results: { player: :player_profile } } },
+            :scores,
+          ])
+          .find_by!(match_number: params[:match_number])
         setup_instance_variables(@match)
       end
     end
