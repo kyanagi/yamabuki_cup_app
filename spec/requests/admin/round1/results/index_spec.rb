3a0aa1670574593802dbf41607f3c2e55319d6ea
Kouhei Yanagita <yanagi@shakenbu.org>
2025-06-07 09:55:27 +0900

    GET /admin/round1/resultsのテストを追加

---
+++ spec/requests/admin/round1/results/index_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "GET /admin/round1/results", type: :request do
+  before do
+    player_profiles = create_list(:player_profile, 10)
+    player_profiles.first(5).each.with_index(1) do |player_profile, i|
+      create(:approximation_quiz_answer, player_id: player_profile.player_id)
+      create(:yontaku_player_paper, player_id: player_profile.player_id)
+      create(:yontaku_player_result, player_id: player_profile.player_id, rank: i, score: 100 - i)
+    end
+  end
+
+  it "returns 200" do
+    subject
+    expect(response).to have_http_status(200)
+  end
+end
