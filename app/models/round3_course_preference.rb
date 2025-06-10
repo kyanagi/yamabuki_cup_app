afc2147fc8eda464373f66fba34019091e987ae3
Kouhei Yanagita <yanagi@shakenbu.org>
2025-06-10 11:40:55 +0900

    Round3CoursePreferenceで、選択したコースに重複がないバリデーションを追加

--- app/models/round3_course_preference.rb
+++ app/models/round3_course_preference.rb
@@ -5,7 +5,19 @@ class Round3CoursePreference < ApplicationRecord
   belongs_to :choice3_match, class_name: "Match"
   belongs_to :choice4_match, class_name: "Match"
 
+  validate :validate_unique_choices
+
+  # @rbs return Array[Match]
   def choices
     [choice1_match, choice2_match, choice3_match, choice4_match]
+  end
+
+  private
+
+  def validate_unique_choices #: void
+    choice_ids = [choice1_match_id, choice2_match_id, choice3_match_id, choice4_match_id].compact
+    if choice_ids.uniq.length != choice_ids.length
+      errors.add(:base, "選択したコースに重複があります")
+    end
   end
 end
--- spec/models/round3_course_preference_spec.rb
+++ spec/models/round3_course_preference_spec.rb
@@ -28,4 +28,44 @@ RSpec.describe Round3CoursePreference, type: :model do
       expect(subject.choices).to eq [matches[1], matches[0], matches[3], matches[2]]
     end
   end
+
+  describe "バリデーション" do
+    let(:round) { Round::ROUND3 }
+    let(:match) { create(:match, round:) }
+
+    context "選択したコースに重複がある場合" do
+      subject do
+        build(
+          :round3_course_preference,
+          choice1_match: match,
+          choice2_match: match,
+          choice3_match: match,
+          choice4_match: match
+        )
+      end
+
+      it "無効であること" do
+        expect(subject).not_to be_valid
+        expect(subject.errors[:base]).to include("選択したコースに重複があります")
+      end
+    end
+
+    context "選択したコースに重複がない場合" do
+      let(:matches) { create_list(:match, 4, round:) }
+
+      subject do
+        build(
+          :round3_course_preference,
+          choice1_match: matches[0],
+          choice2_match: matches[1],
+          choice3_match: matches[2],
+          choice4_match: matches[3]
+        )
+      end
+
+      it "有効であること" do
+        expect(subject).to be_valid
+      end
+    end
+  end
 end
