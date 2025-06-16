78a35d27dd3088249eac8dff3899b53b53eb526d
Kouhei Yanagita <yanagi@shakenbu.org>
2025-06-16 11:27:52 +0900

    Reactを削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from "vite-plugin-ruby";
 import FullReload from "vite-plugin-full-reload";
-import ReactPlugin from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
     FullReload(['config/routes.rb', 'app/views/**/*', 'app/assets/stylesheets/**/*']),
-    ReactPlugin(),
   ],
 })
