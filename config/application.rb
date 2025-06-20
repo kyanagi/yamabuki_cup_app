410c891f9eb213a6001a3530c63552ec00fea444
Kouhei Yanagita <yanagi@shakenbu.org>
2025-06-20 09:35:57 +0900

    正誤操作画面で、1stなどの表記がTranslation missingになっていた問題を修正

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,9 @@ module YamabukiCupApp
     config.time_zone = "Tokyo"
     config.active_record.default_timezone= :local
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.available_locales = [:ja, :en]
+    config.i18n.default_locale = :ja
+    config.i18n.fallbacks = [:ja, :en]
   end
 end
