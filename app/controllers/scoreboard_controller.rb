306605a3530377dbe9f4beca57968585ccdc95b3
Kouhei Yanagita <yanagi@shakenbu.org>
2025-06-21 07:37:10 +0900

    決勝の得点表示を作成

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -5,11 +5,11 @@ class ScoreboardController < ApplicationController
   end
 
   def test
-    match = Round::SEMIFINAL.matches[0]
+    match = Round::FINAL.matches[0]
     @scores = match.current_scores.sort_by { it.matching.seat }
     @score_operation = match.last_score_operation
     render(
-      inline: "<%= content_for(:scoreboard) { render partial: 'scoreboard/board/init', locals: { scores: @scores, score_operation: @score_operation } } %>", # rubocop:disable Layout/LineLength
+      inline: "<%= content_for(:scoreboard) { render partial: 'scoreboard/final/init', locals: { scores: @scores, score_operation: @score_operation } } %>", # rubocop:disable Layout/LineLength
       layout: "scoreboard"
     )
   end
