946a9a016a4a391e8f3075607637e457b4940d45
Kouhei Yanagita <yanagi@shakenbu.org>
2025-06-24 21:40:58 +0900

    Stimulusサンプル用のHelloControllerを削除

--- app/typescript/controllers/admin_index.ts
+++ app/typescript/controllers/admin_index.ts
@@ -1,10 +1,8 @@
 import { application } from "./application";
 import CheckboxHiddenTogglerController from "./checkbox_hidden_toggler_controller";
-import HelloController from "./hello_controller";
 import ModalController from "./modal_controller";
 import QuizReaderController from "./quiz_reader_controller";
 
-application.register("hello", HelloController);
 application.register("modal", ModalController);
 application.register("checkbox-hidden-toggler", CheckboxHiddenTogglerController);
 application.register("quiz-reader", QuizReaderController);
