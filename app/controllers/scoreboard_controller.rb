49cde00e202a9339a6c8912cf10eb1106b037372
Kouhei Yanagita <yanagi@shakenbu.org>
2025-06-25 14:44:56 +0900

    2R発表のデザインを調整

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -5,11 +5,13 @@ class ScoreboardController < ApplicationController
   end
 
   def test
-    match = Round::ROUND3.matches[0]
+    match = Round::ROUND2.matches[0]
+    @ranks = match.matchings.map { |m| m.player.yontaku_player_result.rank }.sort
+
     @scores = match.current_scores.sort_by { it.matching.seat }
     @score_operation = match.last_score_operation
     render(
-      inline: "<%= content_for(:scoreboard) { render partial: 'scoreboard/paper_seed/display' } %>",
+      inline: "<%= content_for(:scoreboard) { render partial: 'scoreboard/round2_announcement/display', locals: { ranks: @ranks } } %>",
       layout: "scoreboard"
     )
   end
