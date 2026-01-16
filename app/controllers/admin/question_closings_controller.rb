0d96db396e37154534b1cfe615fcdeb1661f7dd1
Kouhei Yanagita <yanagi@shakenbu.org>
2026-01-16 22:27:03 +0900

    QuestionClosingでの問題表示送出を廃止

--- app/controllers/admin/question_closings_controller.rb
+++ app/controllers/admin/question_closings_controller.rb
@@ -4,17 +4,14 @@ module Admin
 
     def create
       match = nil
-      question = nil
       ActiveRecord::Base.transaction do
         match = Match
           .preload(last_score_operation: { scores: :matching })
           .find(params[:match_id])
-        qc = QuestionClosing.create!(match:, question_player_results_attributes: create_params)
-        question = qc.question
+        QuestionClosing.create!(match:, question_player_results_attributes: create_params)
       end
 
       broadcast_scoreboard(match)
-      broadcast_question_board(question) if question
       setup_instance_variables(match)
       render "admin/shared/matches/#{@match.rule_class::ADMIN_VIEW_TEMPLATE}/show"
     end
@@ -27,13 +24,6 @@ module Admin
       else
         []
       end
-    end
-
-    def broadcast_question_board(question)
-      ActionCable.server.broadcast(
-        "scoreboard",
-        turbo_stream.update("question") { render_to_string(partial: "scoreboard/question/question", locals: { question: }) }
-      )
     end
   end
 end
